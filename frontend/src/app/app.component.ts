e8c188cfba62ad7fc09181e0ddaf635d55c7bd14
snehalchaudhari123 <snehalchaudhari2002@gmail.com>
2025-12-12 10:33:25 +0530

    complete home and header component-snehal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,38 +1,22 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './pages/header/header.component';
+
 import { CommonModule } from '@angular/common';
-import { BusyMobileComponent } from './pages/pages/busy-mobile/busy-mobile.component';
-import { FooterComponent } from './pages/footer/footer.component';
+
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HeaderComponent,FooterComponent, BusyMobileComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   
   title = 'frontend';
-  isLoginPage = false;
-  showLayout = true;
+  
 
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-
-        // Hide layout (header/footer) on login page
-        this.isLoginPage = this.router.url.includes('login');
-
-        // Hide layout (header/footer) on /busy-mobile
-        this.showLayout = event.url !== '/busy-mobile';
-      }
-    });
-  }
-  currentPage = 'home';
-  goToPage(page: string) {
-    this.currentPage = page;
-  }
 }
 
 
