2b0081c12b898831303f9d1fc51a861aad2dc8a1
snehalchaudhari123 <snehalchaudhari2002@gmail.com>
2025-12-12 10:40:12 +0530

    complete header and home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { BusyMobileComponent } from './pages/pages/busy-mobile/busy-mobile.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  
-  { path: 'login', loadComponent: () => import('./pages/features/auth/login/login.component').then(m => m.LoginComponent) },
-
-  { path: 'busy-mobile', component: BusyMobileComponent },
-  
-  { path: '**', redirectTo: '' },
+    {
+      path:"",component:HomeComponent
+    },
+   // { path: 'home', component: HomeComponent },
+{
+   path:'busy-mobile',component:BusyMobileComponent
+}
 ];
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+
+
+
+  
 
 }
