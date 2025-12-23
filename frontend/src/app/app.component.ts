0b49cdd2ebaedc41278fac82ecfbd2f7b4a4e360
shivrajpowar <shivrajpowar9922@gmail.com>
2025-12-23 16:23:25 +0530

    hide navbar in  busypage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
@@ -12,9 +12,21 @@ import { HeaderComponent } from './shared/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   title = 'BusySoftware';
+   hideHeader=false;
+   constructor(private _route:Router){
+   }
+
+   ngOnInit(): void {
+    this._route.events.subscribe((event)=>{
+      if(event instanceof NavigationEnd){
+        this.hideHeader=event.url.startsWith('/busypage')
+      }
+    })
+     }
+   
   
 
 }
